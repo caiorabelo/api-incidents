fb7a646fb97709350a6d6cf50bd1f9a924da38b9
Caio Rabelo <caiorabelop@gmail.com>
2022-07-02 23:53:33 -0300

    Created migrations

---
+++ database/migrations/2022_06_24_155213_create_incidents_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('incidents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('criticality_id')->constrained('criticalities');
+            $table->foreignId('type_id')->constrained('types');
+            $table->string('title');
+            $table->string('description');
+            $table->boolean('status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('incidents');
+    }
+};
